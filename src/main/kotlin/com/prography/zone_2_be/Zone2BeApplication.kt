fd9509810142447b015f8ace43137c7144f851ec
Lee9Bin <imagen33@daum.net>
2025-05-23 16:19:39 +0900

    initial commit

---
+++ src/main/kotlin/com/prography/zone_2_be/Zone2BeApplication.kt
@@ -0,0 +1,11 @@
+package com.prography.zone_2_be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Zone2BeApplication
+
+fun main(args: Array<String>) {
+	runApplication<Zone2BeApplication>(*args)
+}
---
+++ src/test/kotlin/com/prography/zone_2_be/Zone2BeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.prography.zone_2_be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Zone2BeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
