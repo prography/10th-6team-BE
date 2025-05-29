3e82bb0e4594ef7662bea84b86a8d660bae6b975
Lee9Bin <imagen33@daum.net>
2025-05-29 19:39:40 +0900

    Chore : ErrorCode Enum 클래스 생성

---
+++ src/main/kotlin/com/prography/zone_2_be/global/error/ErrorCode.kt
@@ -0,0 +1,16 @@
+package com.prography.zone_2_be.global.error
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode(
+    val status: HttpStatus,
+    val errorCode: Int,
+    val message: String
+) {
+    //global 에러
+    DEFAULT_ERROR(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        4000,
+        "현재 앱에 문제가 발생했으니 관리자에게 문의해주세요."
+    ),
+}
