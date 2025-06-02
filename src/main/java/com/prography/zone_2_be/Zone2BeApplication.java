cf8817bff72dec7c48a0ae4c2769be792312a702
Lee9Bin <imagen33@daum.net>
2025-06-02 17:33:50 +0900

    initial commit

---
+++ src/main/java/com/prography/zone_2_be/Zone2BeApplication.java
@@ -0,0 +1,13 @@
+package com.prography.zone_2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zone2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zone2BeApplication.class, args);
+	}
+
+}
