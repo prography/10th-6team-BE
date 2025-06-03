8ea518d38c75655ece1f1ee5654f394ef6a69a93
Lee9Bin <imagen33@daum.net>
2025-06-03 23:20:19 +0900

    Feat : Term 엔티티 구현

---
+++ src/main/java/com/prography/zone_2_be/domain/terms/entity/Term.java
@@ -0,0 +1,24 @@
+package com.prography.zone_2_be.domain.terms.entity;
+
+import com.prography.zone_2_be.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Term extends BaseEntity {
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, columnDefinition = "VARCHAR(128)")
+	private TermType termType;
+}
