8e07952f255f84df1f8c5968888e4b46ffcffde4
Lee9Bin <imagen33@daum.net>
2025-06-04 19:07:09 +0900

    Chore : JpaAuditing 활성화

---
+++ src/main/java/com/prography/zone_2_be/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.prography.zone_2_be.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
