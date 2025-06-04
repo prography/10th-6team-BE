78a0d1c64f3de778af528392cbe22103cec7e07c
Lee9Bin <imagen33@daum.net>
2025-06-04 19:51:58 +0900

    Refactor : Long 타입 수정 후 초 단위로 컨버팅

--- src/main/java/com/prography/zone_2_be/global/converter/InstantToEpochSecondConverter.java
+++ src/main/java/com/prography/zone_2_be/global/converter/InstantToEpochSecondConverter.java
@@ -1,20 +1,18 @@
 package com.prography.zone_2_be.global.converter;
 
-import java.time.Instant;
-
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = false)
-public class InstantToEpochSecondConverter implements AttributeConverter<Instant, Long> {
+public class InstantToEpochSecondConverter implements AttributeConverter<Long, Long> {
 
 	@Override
-	public Long convertToDatabaseColumn(Instant attribute) {
-		return (attribute == null ? null : attribute.getEpochSecond());
+	public Long convertToDatabaseColumn(Long attribute) {
+		return (attribute == null ? null : attribute / 1000);
 	}
 
 	@Override
-	public Instant convertToEntityAttribute(Long dbData) {
-		return (dbData == null ? null : Instant.ofEpochSecond(dbData));
+	public Long convertToEntityAttribute(Long dbData) {
+		return (dbData);
 	}
 }
