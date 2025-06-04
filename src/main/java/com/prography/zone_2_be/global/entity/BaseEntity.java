41c701aca7e74a536a0aeba03895b50a79353766
Lee9Bin <imagen33@daum.net>
2025-06-04 19:06:50 +0900

    Refactor : 추상 클래스로 변경 및 @CreatedDate 설정

--- src/main/java/com/prography/zone_2_be/global/entity/BaseEntity.java
+++ src/main/java/com/prography/zone_2_be/global/entity/BaseEntity.java
@@ -2,42 +2,32 @@ package com.prography.zone_2_be.global.entity;
 
 import java.time.Instant;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.prography.zone_2_be.global.converter.InstantToEpochSecondConverter;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false, updatable = false)
-	private Long createdAt;
+	@CreatedDate
+	@Column(nullable = false, updatable = false, columnDefinition = "BIGINT")
+	@Convert(converter = InstantToEpochSecondConverter.class)
+	private Instant createdAt;
 
-	@Column(nullable = false)
-	private Long updatedAt;
-
-	@PrePersist
-	protected void prePersist() {
-		long now = Instant.now().toEpochMilli();
-		this.createdAt = now;
-		this.updatedAt = now;
-	}
-
-	@PreUpdate
-	protected void preUpdate() {
-		this.updatedAt = Instant.now().toEpochMilli();
-	}
 }
