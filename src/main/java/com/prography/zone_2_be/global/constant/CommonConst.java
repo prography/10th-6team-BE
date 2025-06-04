83c49d3962626f7cc050ab89dec7ce8bff1630dd
Lee9Bin <imagen33@daum.net>
2025-06-05 02:03:43 +0900

    Feat : 상수 관리 클래스 구현

---
+++ src/main/java/com/prography/zone_2_be/global/constant/CommonConst.java
@@ -0,0 +1,9 @@
+package com.prography.zone_2_be.global.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CommonConst {
+	public static final int DEFAULT_PAGE_SIZE = 10;
+}
