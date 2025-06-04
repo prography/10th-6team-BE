800f392b5d2ffdfff66f67c6a3e2369443159d0d
Lee9Bin <imagen33@daum.net>
2025-06-04 19:06:59 +0900

    Chore : UpdatableEntity 구현

---
+++ src/main/java/com/prography/zone_2_be/global/entity/UpdatableEntity.java
@@ -0,0 +1,22 @@
+package com.prography.zone_2_be.global.entity;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.prography.zone_2_be.global.converter.InstantToEpochSecondConverter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class UpdatableEntity extends BaseEntity {
+
+	@LastModifiedDate
+	@Column(nullable = false, columnDefinition = "BIGINT")
+	@Convert(converter = InstantToEpochSecondConverter.class)
+	private Instant updatedAt;
+}
