c633b0b89f1b7ccae161586c8a1cb873086221d3
Lee9Bin <imagen33@daum.net>
2025-06-05 02:40:14 +0900

    Feat : Notice 목록 조회 API 구현

---
+++ src/main/java/com/prography/zone_2_be/domain/notice/controller/NoticeController.java
@@ -0,0 +1,34 @@
+package com.prography.zone_2_be.domain.notice.controller;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prography.zone_2_be.domain.notice.dto.NoticeResponseDto;
+import com.prography.zone_2_be.domain.notice.service.NoticeService;
+import com.prography.zone_2_be.global.constant.CommonConst;
+import com.prography.zone_2_be.global.response.ApiResponse;
+import com.prography.zone_2_be.global.response.SliceResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/notice")
+@RequiredArgsConstructor
+public class NoticeController {
+
+	private final NoticeService noticeService;
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<SliceResponse<NoticeResponseDto>>> findAllNotice(
+		@RequestParam(defaultValue = "0") int pageNumber) {
+		Pageable pageable = PageRequest.of(pageNumber, CommonConst.DEFAULT_PAGE_SIZE);
+		SliceResponse<NoticeResponseDto> response = noticeService.findAllNotice(pageable);
+
+		return ApiResponse.success(response);
+	}
+}
