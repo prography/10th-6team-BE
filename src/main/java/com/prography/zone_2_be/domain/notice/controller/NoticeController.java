befc8ae6d99cd7ecab8a9cd8010479d42f196a8b
Lee9Bin <imagen33@daum.net>
2025-06-05 21:34:06 +0900

    Feat : Notice 상세 조회 API 구현

--- src/main/java/com/prography/zone_2_be/domain/notice/controller/NoticeController.java
+++ src/main/java/com/prography/zone_2_be/domain/notice/controller/NoticeController.java
@@ -4,11 +4,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prography.zone_2_be.domain.notice.dto.NoticeFindAllResponse;
+import com.prography.zone_2_be.domain.notice.dto.NoticeFindResponse;
 import com.prography.zone_2_be.domain.notice.service.NoticeService;
 import com.prography.zone_2_be.global.constant.CommonConst;
 import com.prography.zone_2_be.global.response.ApiResponse;
@@ -29,6 +31,13 @@ public class NoticeController {
 		Pageable pageable = PageRequest.of(pageNumber, CommonConst.DEFAULT_PAGE_SIZE);
 		SliceResponse<NoticeFindAllResponse> response = noticeService.findAllNotice(pageable);
 
+		return ApiResponse.success(response);
+	}
+
+	@GetMapping("/{noticeId}")
+	public ResponseEntity<ApiResponse<NoticeFindResponse>> findNotice(
+		@PathVariable("noticeId") Long noticeId) {
+		NoticeFindResponse response = noticeService.findNotice(noticeId);
 		return ApiResponse.success(response);
 	}
 }
