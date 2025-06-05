4737df48f95e9bd1e659e083b0ce366b89ad2c84
Lee9Bin <imagen33@daum.net>
2025-06-05 21:33:29 +0900

    Feat : 엔티티 생성 메서드 구현

--- src/main/java/com/prography/zone_2_be/domain/notice/entity/Notice.java
+++ src/main/java/com/prography/zone_2_be/domain/notice/entity/Notice.java
@@ -5,6 +5,7 @@ import com.prography.zone_2_be.global.entity.UpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,4 +19,17 @@ public class Notice extends UpdatableEntity {
 
 	@Column(nullable = false, columnDefinition = "TEXT")
 	private String content;
+
+	@Builder
+	private Notice(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public static Notice of(String title, String content) {
+		return Notice.builder()
+			.title(title)
+			.content(content)
+			.build();
+	}
 }
