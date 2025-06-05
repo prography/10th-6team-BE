12842fcb1a5ae936bfe61fb5f7cd63e1344e84c6
Lee9Bin <imagen33@daum.net>
2025-06-05 21:33:07 +0900

    Feat : findById 디폴트 메서드 구현

--- src/main/java/com/prography/zone_2_be/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/prography/zone_2_be/domain/notice/repository/NoticeRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.prography.zone_2_be.domain.notice.entity.Notice;
+import com.prography.zone_2_be.global.error.ErrorCode;
+import com.prography.zone_2_be.global.exception.CustomException;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+	//default 메서드
+	default Notice findByIdOrThrow(Long noticeId) {
+		return findById(noticeId)
+			.orElseThrow(() -> new CustomException(ErrorCode.NOTICE_NOT_FOUND));
+	}
 
 	Slice<Notice> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
