65a1512e5ebd2a4ead408c9f86fb6e2f314f6512
Lee9Bin <imagen33@daum.net>
2025-06-05 21:33:50 +0900

    Feat : findNotice 메서드 구현

--- src/main/java/com/prography/zone_2_be/domain/notice/service/NoticeService.java
+++ src/main/java/com/prography/zone_2_be/domain/notice/service/NoticeService.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 import com.prography.zone_2_be.domain.notice.dto.NoticeFindAllResponse;
+import com.prography.zone_2_be.domain.notice.dto.NoticeFindResponse;
+import com.prography.zone_2_be.domain.notice.entity.Notice;
 import com.prography.zone_2_be.domain.notice.repository.NoticeRepository;
 import com.prography.zone_2_be.global.response.SliceResponse;
 
@@ -20,5 +22,10 @@ public class NoticeService {
 		Slice<NoticeFindAllResponse> noticeResponse = noticeRepository.findAllByOrderByCreatedAtDesc(pageable)
 			.map(NoticeFindAllResponse::from);
 		return SliceResponse.from(noticeResponse);
+	}
+
+	public NoticeFindResponse findNotice(Long noticeId) {
+		Notice notice = noticeRepository.findByIdOrThrow(noticeId);
+		return NoticeFindResponse.from(notice);
 	}
 }
