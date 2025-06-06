263247d58dc0e4af34eaf28b94a212be3b0dd429
yxnxj <micayel1219@gmail.com>
2025-06-06 17:21:16 +0900

    Feat : authkey인증 후 토큰 발급시 새로운 유저인지 전달 및 save

--- src/main/java/com/prography/zone_2_be/domain/auth/dto/AuthRequestDto.java
+++ src/main/java/com/prography/zone_2_be/domain/auth/dto/AuthRequestDto.java
@@ -1,9 +1,14 @@
 package com.prography.zone_2_be.domain.auth.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public class AuthRequestDto {
     @NotNull
     public String oAuth2Key;
+
+    @NotNull
+    @Email
+    public String email;
 }
 
