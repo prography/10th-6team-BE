5ac10bb5cb88a903db65c618c4aab552d1ac9806
Lee9Bin <imagen33@daum.net>
2025-06-06 21:58:53 +0900

    Feat : findById 디폴트 메서드 구현

--- src/main/java/com/prography/zone_2_be/domain/customerinquiry/repository/CustomerInquiryRepository.java
+++ src/main/java/com/prography/zone_2_be/domain/customerinquiry/repository/CustomerInquiryRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.prography.zone_2_be.domain.customerinquiry.entity.CustomerInquiry;
+import com.prography.zone_2_be.global.error.ErrorCode;
+import com.prography.zone_2_be.global.exception.CustomException;
 
 @Repository
 public interface CustomerInquiryRepository extends JpaRepository<CustomerInquiry, Long> {
+
+	//default 메서드
+	default CustomerInquiry findByIdOrThrow(Long customerInquiryId) {
+		return findById(customerInquiryId)
+			.orElseThrow(() -> new CustomException(ErrorCode.DEFAULT_ERROR));
+	}
 
 	Slice<CustomerInquiry> findAllByOrderByIsPinnedDescCreatedAtDesc(Pageable pageable);
 }
