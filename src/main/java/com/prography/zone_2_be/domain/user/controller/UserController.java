7ad288560f49f17c611adbaa6afe7e5a8a28d46b
yxnxj <micayel1219@gmail.com>
2025-06-08 17:25:56 +0900

    Feat : 회원 조회 api add

--- src/main/java/com/prography/zone_2_be/domain/user/controller/UserController.java
+++ src/main/java/com/prography/zone_2_be/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.prography.zone_2_be.domain.user.controller;
 
+import com.prography.zone_2_be.domain.user.dto.UserFindResponse;
 import com.prography.zone_2_be.domain.user.dto.UserUpdateRequest;
 import com.prography.zone_2_be.domain.user.service.UserService;
 import com.prography.zone_2_be.global.response.ApiResponse;
@@ -7,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +21,10 @@ public class UserController {
     public ResponseEntity<ApiResponse<Void>> updateUser(@Valid @RequestBody UserUpdateRequest request) {
         userService.updateUser(request);
         return ApiResponse.success();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<UserFindResponse>> findUser() {
+        return ApiResponse.success(userService.findUser());
     }
 }
