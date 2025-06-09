605b360faaaf0efe8ddf671af970d56a3a66e4bd
Lee9Bin <imagen33@daum.net>
2025-06-09 21:00:04 +0900

    Feat : TermController에 단건 조회 엔드포인트 추가

--- src/main/java/com/prography/zone_2_be/domain/term/controller/TermController.java
+++ src/main/java/com/prography/zone_2_be/domain/term/controller/TermController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prography.zone_2_be.domain.term.dto.TermFindAllResponse;
 import com.prography.zone_2_be.domain.term.dto.TermFindAllVersionResponse;
+import com.prography.zone_2_be.domain.term.dto.TermFindResponse;
 import com.prography.zone_2_be.domain.term.entity.TermType;
 import com.prography.zone_2_be.domain.term.service.TermService;
 import com.prography.zone_2_be.global.response.ApiResponse;
@@ -28,11 +30,18 @@ public class TermController {
 		List<TermFindAllResponse> terms = termService.findAllTerm();
 		return ApiResponse.success(terms);
 	}
-	
+
 	@GetMapping("/versions")
 	public ResponseEntity<ApiResponse<List<TermFindAllVersionResponse>>> findAllTermVersions(
 		@RequestParam(required = false) TermType termType) {
 		List<TermFindAllVersionResponse> response = termService.findAllTermVersion(termType);
+		return ApiResponse.success(response);
+	}
+
+	@GetMapping("/{termId}")
+	public ResponseEntity<ApiResponse<TermFindResponse>> findTerm(
+		@PathVariable("termId") Long termId) {
+		TermFindResponse response = termService.findTerm(termId);
 		return ApiResponse.success(response);
 	}
 }
