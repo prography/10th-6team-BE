adf418d48d1eb841be0ec4192948c3f84bba3bec
Lee9Bin <imagen33@daum.net>
2025-06-09 19:48:40 +0900

    Feat : TermType별 최신 Term 1건 조회 쿼리 추가

--- src/main/java/com/prography/zone_2_be/domain/term/repository/TermRepository.java
+++ src/main/java/com/prography/zone_2_be/domain/term/repository/TermRepository.java
@@ -3,6 +3,7 @@ package com.prography.zone_2_be.domain.term.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.prography.zone_2_be.domain.term.entity.Term;
@@ -10,5 +11,14 @@ import com.prography.zone_2_be.domain.term.entity.Term;
 @Repository
 public interface TermRepository extends JpaRepository<Term, Long> {
 
-	List<Term> getTerms();
+	@Query(value = """
+		SELECT t.*
+		FROM term t
+		JOIN (
+		    SELECT term_type, MAX(created_at) AS max_created
+		    FROM term
+		    GROUP BY term_type
+		) m ON t.term_type = m.term_type AND t.created_at = m.max_created
+		""", nativeQuery = true)
+	List<Term> findLatestTermsGroupedByType();
 }
