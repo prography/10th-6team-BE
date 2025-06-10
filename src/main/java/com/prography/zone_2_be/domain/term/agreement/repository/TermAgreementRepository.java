587a88d380c8c588f071e378a3ec7442c6214d83
Lee9Bin <imagen33@daum.net>
2025-06-11 02:25:33 +0900

    Feat : User 기준으로 약관 동의 내역 조회 메서드 추가

--- src/main/java/com/prography/zone_2_be/domain/term/agreement/repository/TermAgreementRepository.java
+++ src/main/java/com/prography/zone_2_be/domain/term/agreement/repository/TermAgreementRepository.java
@@ -1,10 +1,16 @@
 package com.prography.zone_2_be.domain.term.agreement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.prography.zone_2_be.domain.term.agreement.entity.TermAgreement;
+import com.prography.zone_2_be.domain.term.entity.Term;
+import com.prography.zone_2_be.domain.user.entity.User;
 
 @Repository
 public interface TermAgreementRepository extends JpaRepository<TermAgreement, Long> {
+	
+	List<TermAgreement> findByUserAndTermIn(User user, List<Term> terms);
 }
