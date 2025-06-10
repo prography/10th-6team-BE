d0d590aa42351eeff1be7164302d2fbed6ec6898
Lee9Bin <imagen33@daum.net>
2025-06-11 03:03:01 +0900

    Feat : 약관 동의 여부 조회 API 구현

--- src/main/java/com/prography/zone_2_be/domain/term/agreement/controller/TermAgreementController.java
+++ src/main/java/com/prography/zone_2_be/domain/term/agreement/controller/TermAgreementController.java
@@ -1,11 +1,16 @@
 package com.prography.zone_2_be.domain.term.agreement.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prography.zone_2_be.domain.term.agreement.dto.TermAgreementFindResponse;
 import com.prography.zone_2_be.domain.term.agreement.dto.TermAgreementSaveAllRequest;
 import com.prography.zone_2_be.domain.term.agreement.service.TermAgreementService;
 import com.prography.zone_2_be.global.response.ApiResponse;
@@ -27,5 +32,14 @@ public class TermAgreementController {
 		String uuid = JwtUtil.getUuid();
 		termAgreementService.saveAllTermAgreement(uuid, request.getTermAgreementSaveRequests());
 		return ApiResponse.success();
+	}
+
+	@GetMapping("/status")
+	public ResponseEntity<ApiResponse<List<TermAgreementFindResponse>>> getTermAgreementStatus(
+		@RequestParam List<Long> termIds
+	) {
+		String uuid = JwtUtil.getUuid();
+		List<TermAgreementFindResponse> response = termAgreementService.getTermAgreementStatus(uuid, termIds);
+		return ApiResponse.success(response);
 	}
 }
