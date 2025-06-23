0e659dda32c67dd97a8398fd278acb45406eb0b9
YeonJun Cho <micayel1219@gmail.com>
2025-06-23 20:45:17 +0900

    Chore : 주석 정리

--- src/main/java/com/prography/zone_2_be/domain/workout/dto/WorkoutGetHistoryResponse.java
+++ src/main/java/com/prography/zone_2_be/domain/workout/dto/WorkoutGetHistoryResponse.java
@@ -1,21 +1,20 @@
 // com.prography.zone_2_be.domain.workout.dto/WorkoutGetHistoryResponse.java
 package com.prography.zone_2_be.domain.workout.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor; // Lombok의 @RequiredArgsConstructor 대신 AllArgsConstructor를 쓰거나 NoArgsConstructor 추가
-import lombok.AllArgsConstructor; // 모든 필드를 인자로 받는 생성자
-
 import java.util.List;
 
-@Getter
-@NoArgsConstructor // Lombok 어노테이션 사용 시 필요
-@AllArgsConstructor // Lombok 어노테이션 사용 시 필요
-public class WorkoutGetHistoryResponse {
-    // WorkoutTotalSumsDto에서 가져올 필드들
-    private Long totalExecTime;
-    private Long totalKcalUsage;
-    private Long totalZone2FatUsage; // DTO 필드명은 유지
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-    // 개별 운동 기록 DTO 리스트
-    private List<WorkoutHistoryDto> histories; // 분리된 DTO 사용
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkoutGetHistoryResponse {
+	private Long totalExecTime;
+	private Long totalKcalUsage;
+	private Long totalZone2FatUsage;
+
+	// 개별 운동 기록 DTO 리스트
+	private List<WorkoutHistoryDto> histories;
 }
