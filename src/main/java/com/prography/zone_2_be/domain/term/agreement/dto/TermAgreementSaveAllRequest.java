f17dd692b1a50a54023d1162dcf950e6490475e6
Lee9Bin <imagen33@daum.net>
2025-06-28 23:07:00 +0900

    Feat : TermAgreementSaveAllRequest, TermAgreementSaveRequest DTO에 Validation 적용

--- src/main/java/com/prography/zone_2_be/domain/term/agreement/dto/TermAgreementSaveAllRequest.java
+++ src/main/java/com/prography/zone_2_be/domain/term/agreement/dto/TermAgreementSaveAllRequest.java
@@ -2,6 +2,7 @@ package com.prography.zone_2_be.domain.term.agreement.dto;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TermAgreementSaveAllRequest {
+
+	@Valid
 	private List<TermAgreementSaveRequest> termAgreementSaveRequests;
 }
--- src/main/java/com/prography/zone_2_be/domain/term/agreement/dto/TermAgreementSaveRequest.java
+++ src/main/java/com/prography/zone_2_be/domain/term/agreement/dto/TermAgreementSaveRequest.java
@@ -1,5 +1,7 @@
 package com.prography.zone_2_be.domain.term.agreement.dto;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TermAgreementSaveRequest {
+
+	@NotNull(message = "termId는 필수입니다.")
 	private Long termId;
+
+	@AssertTrue(message = "약관에 동의하지 않으면 진행할 수 없습니다.")
 	private boolean agreed;
 }
