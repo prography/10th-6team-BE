e512b657a6eab2b580fc300460c73e210748604d
Lee9Bin <imagen33@daum.net>
2025-06-29 03:57:57 +0900

    Feat : UserAuthRequest DTO에 registrationId 필수 필드 추가

--- src/main/java/com/prography/zone_2_be/domain/auth/dto/UserAuthRequest.java
+++ src/main/java/com/prography/zone_2_be/domain/auth/dto/UserAuthRequest.java
@@ -1,9 +1,13 @@
 package com.prography.zone_2_be.domain.auth.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
+@Getter
 public class UserAuthRequest {
-    @NotNull(message = "OAuth2 키는 필수 입력값입니다.")
-    public String oauth2Key;
+	@NotNull(message = "OAuth2 키는 필수 입력값입니다.")
+	public String oauth2Key;
+	@NotNull(message = "registrationId 필수 입력값입니다.")
+	public String registrationId;
 }
 
