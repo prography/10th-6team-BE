740d00b8de4260af2d7c9233622815aa0cfd24f9
Lee9Bin <imagen33@daum.net>
2025-06-28 20:47:11 +0900

    Feat : Workout.of 정적 팩토리 메서드 추가

--- src/main/java/com/prography/zone_2_be/domain/workout/entity/Workout.java
+++ src/main/java/com/prography/zone_2_be/domain/workout/entity/Workout.java
@@ -1,12 +1,20 @@
 package com.prography.zone_2_be.domain.workout.entity;
 
-import java.util.UUID;
-
 import com.prography.zone_2_be.domain.user.entity.User;
 import com.prography.zone_2_be.global.entity.BaseEntity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -37,7 +45,8 @@ public class Workout extends BaseEntity {
 	private Activity activity;
 
 	@Builder
-	private Workout(User user, String uuid, long execTime, int kcalUsage, int fatUsage, int zone2Rate, Activity activity) {
+	private Workout(User user, String uuid, long execTime, int kcalUsage, int fatUsage, int zone2Rate,
+		Activity activity) {
 		this.user = user;
 		this.uuid = uuid;
 		this.execTime = execTime;
@@ -45,6 +54,19 @@ public class Workout extends BaseEntity {
 		this.fatUsage = fatUsage;
 		this.zone2Rate = zone2Rate;
 		this.activity = activity;
+	}
+
+	public static Workout of(User user, String uuid, long execTime, int kcalUsage, int fatUsage, int zone2Rate,
+		Activity activity) {
+		return Workout.builder()
+			.user(user)
+			.uuid(uuid)
+			.execTime(execTime)
+			.kcalUsage(kcalUsage)
+			.fatUsage(fatUsage)
+			.zone2Rate(zone2Rate)
+			.activity(activity)
+			.build();
 	}
 
 }
