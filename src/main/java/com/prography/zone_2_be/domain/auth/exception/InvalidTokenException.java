93ac303a3a0ce75f703bccb361c4ea38000e3797
yxnxj <micayel1219@gmail.com>
2025-06-29 04:05:54 +0900

    Feat : token exception global handler add

--- src/main/java/com/prography/zone_2_be/domain/auth/exception/InvalidTokenException.java
+++ src/main/java/com/prography/zone_2_be/domain/auth/exception/InvalidTokenException.java
@@ -16,13 +16,18 @@ public class InvalidTokenException extends AuthenticationException {
 
 	// 기본 생성자
 	public InvalidTokenException() {
-		super("유효하지 않은 액세스 토큰입니다."); // 부모 클래스에 에러 메시지 전달
+		super("유효하지 않은 토큰입니다."); // 부모 클래스에 에러 메시지 전달
 		this.errorCode = ErrorCode.INVALID_TOKEN; // 기본 에러 코드 설정
 	}
 
 	// ErrorCode를 받는 생성자
-	public InvalidTokenException(ErrorCode errorCode) {
-		super(errorCode.getMessage()); // 부모 클래스에 에러 메시지 전달
+	public InvalidTokenException(String message) {
+		super(message); // 부모 클래스에 에러 메시지 전달
+		this.errorCode = ErrorCode.INVALID_TOKEN;
+	}
+
+	public InvalidTokenException(ErrorCode errorCode, String message) {
+		super(message); // 부모 클래스에 에러 메시지 전달
 		this.errorCode = errorCode;
 	}
 }
