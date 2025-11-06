102bd62f42b0c0a0251c3f17032c00f8b6f63684
Lee9Bin <imagen33@daum.net>
2025-11-06 22:16:07 +0900

    Feat : WorkoutGetResultResponse에 uuid, activity 필드 추가 및 매핑 로직 반영

--- src/main/java/com/prography/zone_2_be/domain/workout/dto/WorkoutGetResultResponse.java
+++ src/main/java/com/prography/zone_2_be/domain/workout/dto/WorkoutGetResultResponse.java
@@ -1,35 +1,40 @@
 package com.prography.zone_2_be.domain.workout.dto;
 
-
 import com.prography.zone_2_be.domain.workout.entity.Workout;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Builder
 @AllArgsConstructor
 @Getter
 public class WorkoutGetResultResponse {
-    private Long execTime;
-    private Integer zone2Rate;
-    private Integer kcalUsage;
-    private Integer relativeFatUsage;
-    private Integer zone2FatUsage;
-    private Integer zone4FatUsage;
-    private Integer foodFigure;
-    private Long createdAt;
 
-    public static WorkoutGetResultResponse from(Workout workout, Integer relativeFatUsage, Integer zone2FatUsage, Integer zone4FatUsage, FoodFigure foodFigure) {
-        return WorkoutGetResultResponse.builder()
-                .execTime(workout.getExecTime())
-                .zone2Rate(workout.getZone2Rate())
-                .kcalUsage(workout.getKcalUsage())
-                .relativeFatUsage(relativeFatUsage)
-                .zone2FatUsage(zone2FatUsage)
-                .zone4FatUsage(zone4FatUsage)
-                .foodFigure(foodFigure.getValue())
-                .createdAt(workout.getCreatedAt().getEpochSecond())
-                .build();
-    }
+	private String uuid;
+	private Long execTime;
+	private Integer zone2Rate;
+	private Integer kcalUsage;
+	private Integer relativeFatUsage;
+	private Integer zone2FatUsage;
+	private Integer zone4FatUsage;
+	private Integer foodFigure;
+	private int activity;
+	private Long createdAt;
+
+	public static WorkoutGetResultResponse from(Workout workout, Integer relativeFatUsage, Integer zone2FatUsage,
+		Integer zone4FatUsage, FoodFigure foodFigure) {
+		return WorkoutGetResultResponse.builder()
+			.uuid(workout.getUuid())
+			.activity(workout.getActivity().getValue())
+			.execTime(workout.getExecTime())
+			.zone2Rate(workout.getZone2Rate())
+			.kcalUsage(workout.getKcalUsage())
+			.relativeFatUsage(relativeFatUsage)
+			.zone2FatUsage(zone2FatUsage)
+			.zone4FatUsage(zone4FatUsage)
+			.foodFigure(foodFigure.getValue())
+			.createdAt(workout.getCreatedAt().getEpochSecond())
+			.build();
+	}
 }
