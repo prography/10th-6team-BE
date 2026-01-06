2db1bbec43f9927e00dd74667b32d95529f69039
Lee9Bin <imagen33@daum.net>
2026-01-07 02:47:58 +0900

    Feat : Alarm 설정 기능
    - 회원 가입 시 알림 설정 초기화 구현

--- src/main/java/com/prography/zone_2_be/domain/alarm/service/AlarmService.java
+++ src/main/java/com/prography/zone_2_be/domain/alarm/service/AlarmService.java
@@ -1,8 +1,14 @@
 package com.prography.zone_2_be.domain.alarm.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prography.zone_2_be.domain.alarm.entity.Alarm;
+import com.prography.zone_2_be.domain.alarm.entity.AlarmType;
 import com.prography.zone_2_be.domain.alarm.repository.AlarmRepository;
+import com.prography.zone_2_be.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +17,18 @@ import lombok.RequiredArgsConstructor;
 public class AlarmService {
 
 	private final AlarmRepository alarmRepository;
+
+	/**
+	 * 회원가입 시 알림 설정 초기화
+	 */
+	@Transactional
+	public void initializeAlarmByUser(User user) {
+
+		List<Alarm> alarms = AlarmType.getAllTypes().stream()
+			.map(type -> Alarm.of(user, type))
+			.toList();
+
+		alarmRepository.saveAll(alarms);
+	}
 
 }
